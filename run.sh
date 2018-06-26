e715e8eabea839f87a04d859b20f265569ef7872
starryard <lytkzx@163.com>
2018-06-26 09:04:59 +0800

    update run.sh add prd and dev mode

--- run.sh
+++ run.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
-export ENVIRONMENT=prd
-echo "当前环境为$ENVIRONMENT"
+if [ -n "$2" ] ;
+then
+    echo "参数中提供了环境信息"
+    PROFILE="$2"
+else
+    PROFILE=dev
+fi
 
-logfile=/var/log/starrynight.log
-touch $logfile
+echo "当前环境配置为$PROFILE"
+
+logfile=/var/tmp/starrynight.log
 
 start(){
-    nohup java -jar target/starrynight-0.0.1.jar -Dspring.profiles.active=prd > $logfile  2>&1 &
+    # nohup java -jar target/starrynight-0.0.1.jar -Dspring.profiles.active=$PROFILE > $logfile  2>&1 &
+    nohup mvn spring-boot:run -Dmaven.test.skip=true -Dspring.profiles.active=$PROFILE > $logfile  2>&1 &
+    touch $logfile
     tailf $logfile
 }
 
@@ -31,7 +39,7 @@ stop(){
     fi
 }
 help(){
-    echo "Usage: start|stop|restart"
+    echo "Usage: (start|stop|restart) [dev|prd]"
 }
 
 case $1 in
